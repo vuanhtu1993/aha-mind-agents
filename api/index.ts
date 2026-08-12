47dca5343b7c425f2296f331c8f5f035d6a1ca4c
vuanhtu1993 <vuanhtu1993mta@gmail.com>
2026-08-12 09:39:34 +0700

    Phase 07: Fix issue build error

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as express from 'express';
+import express from 'express';
 import { Express, Request, Response } from 'express';
 import { AppModule } from '../src/app.module';
 
